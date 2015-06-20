b2c0e0a0f626caa4cffce4a078e6607b2b5d25ae
leroyg <leroy@gardner.io>
2015-06-19 19:41:03 -0500

    deploying photo-ember-api

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
@@ -11,6 +12,12 @@ App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   Resolver: Resolver
 });
+
+DS.RESTAdapter.reopen({
+  namespace: "api",
+  domain: "http://photo-ember-api.herkouapp.com",
+
+})
 
 loadInitializers(App, config.modulePrefix);
 
---
+++ app/models/photo.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  large_photo_url: DS.attr('string'),
+  thumb_photo_url: DS.attr('string'),
+  name: DS.attr('string')
+
+
+});
